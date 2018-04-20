abcd660f198783245a8ffc511ca0d137e07614f3
Nikita Shumakov <shumakov.n@yandex.ru>
2018-04-20 21:54:39 +0500

    up

---
+++ public/javascripts/common.js
@@ -0,0 +1,2 @@
+"use strict";$(function(){$(document).ready(function(){$(".slider").slick({dots:!0,infinite:!1,speed:300,slidesToShow:3,slidesToScroll:3,responsive:[{breakpoint:1024,settings:{slidesToShow:3,slidesToScroll:3,infinite:!0,dots:!0,arrows:!1}},{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:2,arrows:!1}},{breakpoint:480,settings:{slidesToShow:2,slidesToScroll:2,arrows:!1}}]})})});
+//# sourceMappingURL=../javascripts/common.js.map
